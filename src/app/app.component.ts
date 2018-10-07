7c55cc9338df28a5f1e64b1f604246f95dc7ed1e
Daynil <dlibinrx@gmail.com>
2018-10-07 11:33:36 -0500

    footer fix, route transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   copydate: string;
 
+  transitioning = false;
+
+  constructor(private router: Router) {}
+
   ngOnInit() {
     this.copydate = new Date().getFullYear() + '';
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(e => this.transition());
+  }
+
+  transition() {
+    if (this.transitioning) this.transitioning = false;
+    this.transitioning = true;
+    setTimeout(() => (this.transitioning = false), 600);
   }
 
   mail(
