1dc0e746a7d6f50af424c5ec34bc18f21e6bb566
Daynil <dlibinrx@gmail.com>
2018-10-27 14:42:45 -0500

    form on contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -19,4 +19,41 @@ export class ContactComponent implements OnInit {
       }&q=Inner+Path,+Westmont,+IL+60559`
     );
   }
+
+  mail(
+    nameFirst: HTMLInputElement,
+    nameLast: HTMLInputElement,
+    email: HTMLInputElement,
+    body: HTMLTextAreaElement
+  ) {
+    const firstTxt = nameFirst.value;
+    const lastTxt = nameLast.value;
+    const emailTxt = email.value;
+    const subjectTxt = 'Footer contact';
+    const bodyTxt = body.value;
+    const vals = [firstTxt, lastTxt, emailTxt, bodyTxt];
+    let canSubmit = true;
+    // for (let i = 0; i < vals.length; i++) {
+    //   const curVal = vals[i];
+    //   if (curVal.length < 1) {
+    //     this.toast.error(`Please fill out all fields!`);
+    //     canSubmit = false;
+    //     break;
+    //   }
+    // }
+    // if (canSubmit) {
+    //   this.mailService
+    //       .sendEmail(firstTxt, lastTxt,
+    //                  emailTxt, subjectTxt, bodyTxt)
+    //       .then(res => {
+    //         if (res.message === 'sent') {
+    //           this.toast.success('Email sent!');
+    //           nameFirst.value = '';
+    //           nameLast.value = '';
+    //           email.value = '';
+    //           body.value = '';
+    //         }
+    //       });
+    // }
+  }
 }
