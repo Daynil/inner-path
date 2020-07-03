27d02ba42bba472aab89d2d749261c135cb26f02
Daynil <dlibinrx@gmail.com>
2020-07-03 10:22:07 -0500

    remove monique, zip code fix

--- src/app/about/us/us.component.ts
+++ src/app/about/us/us.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Person } from '../about-card.component';
+import { Component, OnInit } from "@angular/core";
+import { Person } from "../about-card.component";
 
 @Component({
-  selector: 'app-us',
-  templateUrl: './us.component.html',
-  styleUrls: ['./us.component.scss']
+  selector: "app-us",
+  templateUrl: "./us.component.html",
+  styleUrls: ["./us.component.scss"],
 })
 export class UsComponent implements OnInit {
   people: Person[];
@@ -14,47 +14,47 @@ export class UsComponent implements OnInit {
   ngOnInit() {
     this.people = [
       new Person(
-        'Shelley',
-        'Pier',
-        'LCSW',
-        'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1474301780/home_mxdrbd.jpg',
-        '../shelley'
+        "Shelley",
+        "Pier",
+        "LCSW",
+        "https://res.cloudinary.com/dz9rf4hwz/image/upload/v1474301780/home_mxdrbd.jpg",
+        "../shelley"
       ),
       new Person(
-        'Eryn',
-        'Smith-Moeller',
-        'LCPC',
-        'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1540147308/innerpath/Eryn_s_pic.png',
-        '../eryn'
+        "Eryn",
+        "Smith-Moeller",
+        "LCPC",
+        "https://res.cloudinary.com/dz9rf4hwz/image/upload/v1540147308/innerpath/Eryn_s_pic.png",
+        "../eryn"
       ),
       new Person(
-        'Asma',
-        'Afridi',
-        'MeD, LCPC, PEL',
-        'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1540148695/innerpath/Asma.jpg',
-        '../asma'
+        "Asma",
+        "Afridi",
+        "MeD, LCPC, PEL",
+        "https://res.cloudinary.com/dz9rf4hwz/image/upload/v1540148695/innerpath/Asma.jpg",
+        "../asma"
+      ),
+      // new Person(
+      //   'Monique',
+      //   'Prohaska-Slattery',
+      //   'LCPC, ATR, CYT',
+      //   'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1540148413/innerpath/Monique_s_pic.jpg',
+      //   '../monique'
+      // ),
+      new Person(
+        "Sue",
+        "Shekut",
+        "LCPC, LMT",
+        "https://res.cloudinary.com/dz9rf4hwz/image/upload/v1547780304/innerpath/SueShekut_7-17_5845_sm.jpg",
+        "../sue"
       ),
       new Person(
-        'Monique',
-        'Prohaska-Slattery',
-        'LCPC, ATR, CYT',
-        'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1540148413/innerpath/Monique_s_pic.jpg',
-        '../monique'
+        "Cassie",
+        "",
+        "Therapy Dog",
+        "https://res.cloudinary.com/dz9rf4hwz/image/upload/v1472417659/FB_IMG_1470266619808_whqzlc.jpg",
+        "../cassie"
       ),
-      new Person(
-        'Sue',
-        'Shekut',
-        'LCPC, LMT',
-        'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1547780304/innerpath/SueShekut_7-17_5845_sm.jpg',
-        '../sue'
-      ),
-      new Person(
-        'Cassie',
-        '',
-        'Therapy Dog',
-        'https://res.cloudinary.com/dz9rf4hwz/image/upload/v1472417659/FB_IMG_1470266619808_whqzlc.jpg',
-        '../cassie'
-      )
     ];
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import {
   AsmaComponent,
   CassieComponent,
   ErynComponent,
-  MoniqueComponent,
   ShelleyComponent,
   SueComponent,
-  UsComponent
-} from './about';
-import { ContactComponent } from './contact/contact.component';
-import { FormsComponent } from './forms/forms.component';
-import { HomeComponent } from './home/home.component';
+  UsComponent,
+} from "./about";
+import { ContactComponent } from "./contact/contact.component";
+import { FormsComponent } from "./forms/forms.component";
+import { HomeComponent } from "./home/home.component";
 import {
   ClinicalConsultationComponent,
   ExpertWitnessComponent,
   LicensureSupervisionComponent,
   TherapistConsultComponent,
-  TrainingWorkshopsComponent
-} from './other-services';
+  TrainingWorkshopsComponent,
+} from "./other-services";
 import {
   AdolescentsComponent,
   AnimalAssistedComponent,
@@ -32,56 +31,54 @@ import {
   LgbtqiapComponent,
   MoodDisordersComponent,
   PreMaritalCounselingComponent,
-  ReikiComponent,
   ShameWorkComponent,
   SrrComponent,
   ToxicRelationshipsComponent,
   TraumaPtsdComponent,
   WorkshopsComponent,
-  YogaComponent
-} from './specialities';
+} from "./specialities";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'forms', component: FormsComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'us', component: UsComponent },
-  { path: 'shelley', component: ShelleyComponent },
-  { path: 'eryn', component: ErynComponent },
-  { path: 'monique', component: MoniqueComponent },
-  { path: 'asma', component: AsmaComponent },
-  { path: 'sue', component: SueComponent },
-  { path: 'cassie', component: CassieComponent },
-  { path: 'animal-assisted-therapy', component: AnimalAssistedComponent },
-  { path: 'emdr', component: EmdrComponent },
-  { path: 'grief', component: GriefComponent },
-  { path: 'ifs-parts-work', component: IfsPartsWorkComponent },
-  { path: 'lgbtqiap', component: LgbtqiapComponent },
-  { path: 'shame-work', component: ShameWorkComponent },
-  { path: 'toxic-relationships', component: ToxicRelationshipsComponent },
-  { path: 'art-therapy', component: ArtTherapyComponent },
-  { path: 'yoga', component: YogaComponent },
-  { path: 'reiki', component: ReikiComponent },
-  { path: 'adolescents', component: AdolescentsComponent },
-  { path: 'cultural-identity', component: CulturalIdentityComponent },
-  { path: 'couples-therapy', component: CouplesTherapyComponent },
-  { path: 'pre-marital-counseling', component: PreMaritalCounselingComponent },
-  { path: 'mood-disorders', component: MoodDisordersComponent },
-  { path: 'trauma-ptsd', component: TraumaPtsdComponent },
-  { path: 'workshops', component: WorkshopsComponent },
-  { path: 'clinical-consultation', component: ClinicalConsultationComponent },
-  { path: 'expert-witness', component: ExpertWitnessComponent },
-  { path: 'licensure-supervision', component: LicensureSupervisionComponent },
-  { path: 'therapist-consult', component: TherapistConsultComponent },
-  { path: 'training-workshops', component: TrainingWorkshopsComponent },
-  { path: 'srr', component: SrrComponent },
-  { path: 'dbt', component: DbtComponent }
+  { path: "", component: HomeComponent },
+  { path: "forms", component: FormsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "us", component: UsComponent },
+  { path: "shelley", component: ShelleyComponent },
+  { path: "eryn", component: ErynComponent },
+  // { path: 'monique', component: MoniqueComponent },
+  { path: "asma", component: AsmaComponent },
+  { path: "sue", component: SueComponent },
+  { path: "cassie", component: CassieComponent },
+  { path: "animal-assisted-therapy", component: AnimalAssistedComponent },
+  { path: "emdr", component: EmdrComponent },
+  { path: "grief", component: GriefComponent },
+  { path: "ifs-parts-work", component: IfsPartsWorkComponent },
+  { path: "lgbtqiap", component: LgbtqiapComponent },
+  { path: "shame-work", component: ShameWorkComponent },
+  { path: "toxic-relationships", component: ToxicRelationshipsComponent },
+  { path: "art-therapy", component: ArtTherapyComponent },
+  // { path: "yoga", component: YogaComponent },
+  // { path: "reiki", component: ReikiComponent },
+  { path: "adolescents", component: AdolescentsComponent },
+  { path: "cultural-identity", component: CulturalIdentityComponent },
+  { path: "couples-therapy", component: CouplesTherapyComponent },
+  { path: "pre-marital-counseling", component: PreMaritalCounselingComponent },
+  { path: "mood-disorders", component: MoodDisordersComponent },
+  { path: "trauma-ptsd", component: TraumaPtsdComponent },
+  { path: "workshops", component: WorkshopsComponent },
+  { path: "clinical-consultation", component: ClinicalConsultationComponent },
+  { path: "expert-witness", component: ExpertWitnessComponent },
+  { path: "licensure-supervision", component: LicensureSupervisionComponent },
+  { path: "therapist-consult", component: TherapistConsultComponent },
+  { path: "training-workshops", component: TrainingWorkshopsComponent },
+  { path: "srr", component: SrrComponent },
+  { path: "dbt", component: DbtComponent },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })
+    RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
